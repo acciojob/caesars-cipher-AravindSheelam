c3c8d787cc4c3a07fae5f63608bcfcbaf78e2b96
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-12 23:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,50 @@
-// Your Script here.
-
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
-let A = "A".charCodeAt(0);
-let Z = "Z".charCodeAt(0);
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+let A = "A".charCodeAt(0); /// 65
+let Z = "Z".charCodeAt(0); // 90
 function rot13(str) {
-	let ans = "" ;
-	for(let i=0;i<str.length;i++)
-		{
-			let currentChar=str[i].charCodeAt(0);
-			if(currentChar <= Z && currentChar >= A){
-				ans += lookup[str[i]]
-			}
-			else{
-				ans += str[i] ;
-			}
-		}
-	return ans ;
-	// Your Result goes here
-  // Only change code below this line
- //return decodedArr
+    let ans = "" ; // "" => "$N#A"
+    // str = "$A#N" => "$N#A"
+    for(let i = 0; i < str.length; i++){
+        // i = 0
+        let currentChar = str[i].charCodeAt(0);
+        if(currentChar <= Z && currentChar >= A){
+            ans += lookup[str[i]]
+        }
+        else {
+            ans += str[i] ;
+        }
+    }
+    return ans ;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+  
+  window.rot13 = rot13;
